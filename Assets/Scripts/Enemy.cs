02734940bff8187a7524e3207dbfb746c650b1c3
sotiriakatara <katarasotiriamria@gmail.com>
2020-10-21 21:38:10 +0300

    3D Tower Defense Game
    Initial Commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Enemy_Movement))]
+public class Enemy : MonoBehaviour
+{
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    private Enemy_Movement enemy;
+
+    private void Start()
+    {
+
+        enemy = GetComponent<Enemy_Movement>();
+        target = Waypoint.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+        }
+
+        enemy.speed = enemy.startSpeed;
+
+    }
+
+    void GetNextWaypoint()
+    {
+        if (wavepointIndex >= Waypoint.points.Length - 1)
+        {
+            EndPath();
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoint.points[wavepointIndex];
+
+
+    // We subtract our position from the target position to create a
+    // Vector that points from our position to the target position
+    // If we reverse the order, our rotation would be 180 degrees off.
+    Vector3 lookVector = target.position - transform.position;
+    Quaternion rotation = Quaternion.LookRotation(lookVector);
+    transform.rotation = rotation;
+    }
+
+    void EndPath()
+    {
+        PlayerStats.Lives--;
+        WaveSpawner.EnemiesAlive--;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool GameIsOver;
+
+    public GameObject gameOverUI;
+    public GameObject completeLevelUI;
+
+    public string nextLevel = "Level02";
+    public int levelToUnlock = 2;
+
+   
+
+
+    void Start()
+    {
+        GameIsOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (GameIsOver)
+            return;
+
+        if (Input.GetKeyDown("e"))
+        {
+            EndGame();
+        }
+        if (PlayerStats.Lives <= 0)
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        GameIsOver = true;
+        gameOverUI.SetActive(true);
+    }
+
+    public void WinLevel()
+    {
+        GameIsOver = true; // camera stop moving
+        completeLevelUI.SetActive(true);
+       
+    }
+
+
+
+}
